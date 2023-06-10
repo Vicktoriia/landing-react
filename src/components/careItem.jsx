357073ceb4f156ecd58d39a6ec43c9d5c7a49eee
Viktoriia Aleksieieva <avl2556655@gmail.com>
2023-06-11 00:20:51 +0300

    add data en de

--- src/components/careItem.jsx
+++ src/components/careItem.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const CareItem = (props) => {
+   const { t } = useTranslation();
   const location = useLocation();
   const navigate = useNavigate();
   const backLink = location.state?.from ?? '/';
@@ -20,13 +22,13 @@ const CareItem = (props) => {
           </button>
         </div>
         <Container>
-           <h2>{props.data ? props.data.title : 'Loading'}</h2>
-          <h4>{props.data ? props.data.description : 'Loading'}</h4>
+           <h2>{props.data ? t(props.data.title) : 'Loading'}</h2>
+          <h4>{props.data ? t(props.data.description) : 'Loading'}</h4>
           <ul>
             {props.data && props.data.sections
               ? props.data.sections.map((section, i) => (
-                  <li key={`${section}-${i}`}><h5 className="mt-4">{section.title}</h5>
-                    <p>{section.content}</p></li>
+                  <li key={`${section}-${i}`}><h5 className="mt-4">{t(section.title)}</h5>
+                    <p>{t(section.content)}</p></li>
                 ))
               : 'Loading'}
           </ul>
--- src/i18n.js
+++ src/i18n.js
@@ -1,55 +1,55 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import Backend from 'i18next-http-backend';
-import LanguageDetector from 'i18next-browser-languagedetector';
-
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    fallbackLng: 'en', // Язык по умолчанию
-    debug: true, // Включить отладку
-
-    interpolation: {
-      escapeValue: false, // Не экранировать строки
-    },
-  });
-
-export default i18n;
-
 // import i18n from 'i18next';
 // import { initReactI18next } from 'react-i18next';
 // import Backend from 'i18next-http-backend';
 // import LanguageDetector from 'i18next-browser-languagedetector';
-
-// import translationEN from './data.en.json';
-// import translationDE from './data.de.json';
-// import translationUK from './data.uk.json';
 
 // i18n
 //   .use(Backend)
 //   .use(LanguageDetector)
 //   .use(initReactI18next)
 //   .init({
-//     fallbackLng: 'en',
-//     debug: true,
-
-//     resources: {
-//       en: {
-//         translation: translationEN,
-//       },
-//       de: {
-//         translation: translationDE,
-//       },
-//       uk: {
-//         translation: translationUK,
-//       },
-//     },
+//     fallbackLng: 'uk', // Язык по умолчанию
+//     debug: true, // Включить отладку
 
 //     interpolation: {
-//       escapeValue: false,
+//       escapeValue: false, // Не экранировать строки
 //     },
 //   });
 
 // export default i18n;
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import translationEN from './data/data.en.json';
+import translationDE from './data/data.de.json';
+import translationUK from './data/data.uk.json';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+
+    resources: {
+      en: {
+        translation: translationEN,
+      },
+      de: {
+        translation: translationDE,
+      },
+      uk: {
+        translation: translationUK,
+      },
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
