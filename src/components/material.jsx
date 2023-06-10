1bc3985a9da338ce9b35b981f022555e0bf7790a
Viktoriia Aleksieieva <105809872+Vicktoriia@users.noreply.github.com>
2023-06-10 17:36:34 +0300

    fix data

--- src/components/material.jsx
+++ src/components/material.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Material = (props) => {
@@ -9,35 +8,35 @@ const Material = (props) => {
   const handleBackClick = () => navigate(backLink);
 
   return (
-    <main>
-      <Container>
-        <button className="btn-back" type="button" onClick={handleBackClick}>
-          <i className="fa fa-arrow-left-long mr-2"></i>
-        </button>
-        <div>
-          <h2>{props.data ? props.data.title : 'Loading'}</h2>
-          <p>{props.data ? props.data.description : 'Loading'}</p>
+    <div className="container">
+      <div className="intro-material">
+        <div className="overlay">
+      <button className="btn-back" type="button" onClick={handleBackClick}>
+        <i className="fa fa-arrow-left-long mr-2"></i>
+      </button>
+      </div>
+       </div>
+      <div>
+        <h2>{props.data ? props.data.title : 'Loading'}</h2>
+        <p>{props.data ? props.data.description : 'Loading'}</p>
 
-          <h4>{props.data ? props.data.qualitiesTitle : 'Loading'}</h4>
-          <ul>
-            {props.data && props.data.qualities
-              ? props.data.qualities.map((quality, index) => (
-                  <li key={index}>{quality}</li>
-                ))
-              : 'Loading'}
-          </ul>
+        <h4>{props.data ? props.data.qualitiesTitle : 'Loading'}</h4>
+        <ul>
+          {props.data && props.data.qualities
+            ? props.data.qualities.map((q, i) => <li key={`${q}-${i}`}>{q}</li>)
+            : 'Loading'}
+        </ul>
 
-          <h4>{props.data ? props.data.advantagesTitle : 'Loading'}</h4>
-          <ul>
-            {props.data && props.data.advantages
-              ? props.data.advantages.map((advantage, index) => (
-                  <li key={index}>{advantage}</li>
-                ))
-              : 'Loading'}
-          </ul>
-        </div>
-      </Container>
-    </main>
+        <h4>{props.data ? props.data.advantagesTitle : 'Loading'}</h4>
+        <ul>
+          {props.data && props.data.advantages
+            ? props.data.advantages.map((a, i) => (
+                <li key={`${a}-${i}`}>{a}</li>
+              ))
+            : 'Loading'}
+        </ul>
+      </div>
+    </div>
   );
 };
 
--- src/page/materials.jsx
+++ src/page/materials.jsx
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import JsonData from '../data/data.json'
+import React, { useEffect, useState } from 'react';
+import JsonData from '../data/data.json';
 import SmoothScroll from 'smooth-scroll';
 import Material from '../components/material';
-
-
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
